54e85fce58af412c5fb2a1de06267f8f0334adb6
hemeanand <hemeanandh@inxsasia.com>
2020-09-17 13:21:53 +0530

    Added Unit test cases

--- src/test/java/Service/PermutationServiceTest.java
+++ src/test/java/Service/PermutationServiceTest.java
@@ -11,19 +11,28 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PermutationServiceTest {
 
-    List<List<Integer>> testArrayList = new ArrayList<List<Integer>>();
-
-
-    PermutationService permutationService = new PermutationService();
-
     @Test
-    void getPermuttaions() {
+    void testGetPermutaions1() {
+        List<List<Integer>> testArrayList = new ArrayList<List<Integer>>();
+        PermutationService permutationService = new PermutationService();
+
         testArrayList = Arrays.asList(Arrays.asList(2, 2, 3), Arrays.asList(2, 3, 2), Arrays.asList(3, 2, 2));
         List<List<Integer>> outputArrayList = permutationService.getPermutaions(new int[]{2, 2, 3});
         assertEquals(outputArrayList, testArrayList);
     }
 
     @Test
-    void printAllRecursive() {
+    void testGetPermutaions2() {
+        List<List<Integer>> testArrayList = new ArrayList<List<Integer>>();
+        PermutationService permutationService = new PermutationService();
+
+        testArrayList = Arrays.asList(Arrays.asList(1, 2, 3, 3), Arrays.asList(1, 3, 2, 3), Arrays.asList(1, 3, 3, 2),
+                Arrays.asList(2, 1, 3, 3), Arrays.asList(2, 3, 1, 3), Arrays.asList(2, 3, 3, 1), Arrays.asList(3, 2, 1, 3),
+                Arrays.asList(3, 2, 3, 1), Arrays.asList(3, 1, 2, 3), Arrays.asList(3, 1, 3, 2),
+                Arrays.asList(3, 3, 1, 2), Arrays.asList(3, 3, 2, 1));
+        List<List<Integer>> outputArrayList = permutationService.getPermutaions(new int[]{1, 2, 3, 3});
+        assertEquals(outputArrayList, testArrayList);
     }
+
+
 }
